82f2eb1581a51b52f0165571c33981a682da6d1a
manuyadav7351 <manuyadav7351@gmail.com>
2023-09-23 21:03:53 +0530

    added first project crypto currency

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import "./project.scss";
 import html from "../../assets/html.svg";
 import js from "../../assets/js.png";
-import css from "../../assets/css.svg";
+import cryptoImg from "../project/projectassets/crypto-analyzer/crypto.png";
 import react from "../../assets/react-js.svg";
+import { Link } from "react-router-dom";
 
 const projects = [
-  { name: "Shusi Restaurent", r: 25, img: react, id: 1 },
-  { name: "Shoes Shopping Site", r: -15, img: js, id: 2 },
-  { name: "Basket a Ball", r: 25, img: html, id: 3 },
-  { name: "Picture TakeUp", r: -15, img: css, id: 4 },
+  { name: "Shusi Restaurent", r: 25, img: react, id: 1, link : "https://crypto-analyzer-omega.vercel.app/"},
+  { name: "Shoes Shopping Site", r: -15, img: js, id: 2, link : "https://crypto-analyzer-omega.vercel.app/" },
+  { name: "Basket a Ball", r: 25, img: html, id: 3, link : "https://crypto-analyzer-omega.vercel.app/" },
+  { name: "Crypto Currency Tracker", r: -15, img: cryptoImg, id: 4 , link : "https://crypto-analyzer-omega.vercel.app/"},
 ];
 
 const Project = () => {
@@ -18,10 +19,12 @@ const Project = () => {
       <div className="container">
 
         {projects.map((project) => (
+          <Link to={project.link} target="_blank">
           <div className="glass" key={project.id} style={{ "--r": `${project.r}` }} data-text={project.name}>
             <button className="rotate">Info</button>
             <img src={project.img} alt="" />
           </div>
+          </Link>
         ))}
 
       </div>
