48eb1f6cf449a1239d116ececf0e0d07bd46bff8
manuyadav7351 <manuyadav7351@gmail.com>
2023-09-27 22:59:43 +0530

    added coffee restaurant project

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -3,11 +3,11 @@ import "./project.scss";
 import html from "../../assets/html.svg";
 import js from "../../assets/js.png";
 import cryptoImg from "../project/projectassets/crypto-analyzer/crypto.png";
-import react from "../../assets/react-js.svg";
+import restaurantImg from "../project/projectassets/restaurant/restaurant.png"
 import { Link } from "react-router-dom";
 
 const projects = [
-  { name: "Shusi Restaurent", r: 25, img: react, id: 1, link : "https://crypto-analyzer-omega.vercel.app/"},
+  { name: "Coffee Restaurent", r: 25, img: restaurantImg, id: 1, link : "https://restaurant-ten-chi.vercel.app/"},
   { name: "Shoes Shopping Site", r: -15, img: js, id: 2, link : "https://crypto-analyzer-omega.vercel.app/" },
   { name: "Basket a Ball", r: 25, img: html, id: 3, link : "https://crypto-analyzer-omega.vercel.app/" },
   { name: "Crypto Currency Tracker", r: -15, img: cryptoImg, id: 4 , link : "https://crypto-analyzer-omega.vercel.app/"},
