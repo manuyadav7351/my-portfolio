ba6d7be275c8eb44f93f34d8a3afc86bbf5f067a
manu yadav <68324469+manuyadav7351@users.noreply.github.com>
2024-09-22 17:48:19 +0000

    completed rope

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,16 +1,60 @@
+import { useState } from "react";
 import "./home.scss";
+import { useEffect } from "react";
 
 const Home = () => {
   const words = ["M", "a", "n", "u Y", "a", "d", "a", "v"];
   const subtitle = ["Working as a Software Developer at payNways"];
 
+  const [dragging , setDragging] = useState(false);
+  const [position , setPosition] = useState(0);
+  const [lightOn, setLightOn] = useState(false);
+  console.log(lightOn); 
+  const maxDragDistance = 20;
+
+  const handleMouseDown = () => {
+    setDragging(true);
+  }
+
+  const handleMouseUp = () => {
+    setDragging(false);
+    if (position === maxDragDistance) {
+      setLightOn(true);  // Turn on the light if fully dragged
+    } else {
+      setLightOn(false); // Turn off the light if not fully dragged
+    }
+  }
+
+  const handleMouseMove = (e) => {
+    const newPosition =  e.clientY - 100;
+    const constrainedPosition = Math.min(newPosition, maxDragDistance);  // Limit drag to 20px
+    setPosition(constrainedPosition > 0 ? constrainedPosition : 0);
+  }
+
+  useEffect(()=>{
+    if (dragging) {
+      document.addEventListener("mousemove", handleMouseMove);
+      document.addEventListener("mouseup", handleMouseUp);
+    } else {
+      // Remove the event listeners when the drag ends
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
+    }
+
+    // Cleanup event listeners on component unmount
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
+    };
+  },[dragging,position]);
+
   return (
     <>
       <div className="lightCord">
-        <div className="lightCordRope"></div>
-        <div className="lightCordHandle"></div>
-        <div className="lightCordHandle1"></div>
-        <div className="lightCordHandle2"></div>
+        <div className="lightCordRope" style={{ transform  :`translateY(${position}px)` }}></div>
+        <div className="lightCordHandle" style={{ transform  :`translateY(${position}px)` }} onMouseDown={handleMouseDown}></div>
+        <div className="lightCordHandle1" style={{ transform  :`translateY(${position}px)` }} onMouseDown={handleMouseDown}></div>
+        <div className="lightCordHandle2" style={{ transform  :`translateY(${position}px)` }} onMouseDown={handleMouseDown}></div>
       </div>
       <div className="home">
         <div className="home_title_border">
