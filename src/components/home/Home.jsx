b975512088810b0cb2a6c39c3cbf7c9553e2c4b0
manu yadav <68324469+manuyadav7351@users.noreply.github.com>
2024-09-22 17:22:40 +0000

    added rope

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,43 +1,51 @@
 import "./home.scss";
 
 const Home = () => {
-  const words = ["M", "a", "n", "u Y","a", "d", "a", "v"];
-  const subtitle = ["Working as a Angular js Intern at payNways"];
+  const words = ["M", "a", "n", "u Y", "a", "d", "a", "v"];
+  const subtitle = ["Working as a Software Developer at payNways"];
 
   return (
-    <div className="home">
-      <div className="home_title_border">
-        <div className="home_title_top">
-          {words.map((words, index) => (
-            <span
-              key={index}
-              style={{ animationDelay: `${index * 0.4}s` }}
-              className="home_title"
-            >
-              {words}
-            </span>
-          ))}
-        </div>
+    <>
+      <div className="lightCord">
+        <div className="lightCordRope"></div>
+        <div className="lightCordHandle"></div>
+        <div className="lightCordHandle1"></div>
+        <div className="lightCordHandle2"></div>
       </div>
-      <div className="home_title_border">
-        <div className="home_subtitle_top">
-          {subtitle.map((word, index) => (
-            <span
-              key={index}
-              style={{ animationDelay: `${index * 0.4}s` }}
-              className="home_subtitle"
-            >
-              {word}
-            </span>
-          ))}
+      <div className="home">
+        <div className="home_title_border">
+          <div className="home_title_top">
+            {words.map((words, index) => (
+              <span
+                key={index}
+                style={{ animationDelay: `${index * 0.4}s` }}
+                className="home_title"
+              >
+                {words}
+              </span>
+            ))}
+          </div>
         </div>
-        {/* <div className="home_subtitle_top">
+        <div className="home_title_border">
+          <div className="home_subtitle_top">
+            {subtitle.map((word, index) => (
+              <span
+                key={index}
+                style={{ animationDelay: `${index * 0.4}s` }}
+                className="home_subtitle"
+              >
+                {word}
+              </span>
+            ))}
+          </div>
+          {/* <div className="home_subtitle_top">
             <span className="home_subtitle" data-text={subtitle} >
               {subtitle}
             </span>
         </div> */}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
